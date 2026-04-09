7f0d62862773bd8b143190b43baa963fa3bf7747
Tommy Grullon <tommycontreras34@gmail.com>
2026-04-09 16:21:29 -0400

    Add main functionality and GitHub action types constant
    - Implement main function to handle user input commands.
    - Add support for "github-activity" and "exit" commands.
    - Introduce GITHUB_ACTION_TYPE constant for event types.

---
+++ constant.js
@@ -0,0 +1,9 @@
+export const GITHUB_ACTION_TYPE = {
+    "WatchEvent": "WATCH",
+    "IssueCommentEvent": "ISSUE_COMMENT",
+    "PullRequestReviewEvent": "PULL_REQUEST_REVIEW",
+    "PullRequestReviewCommentEvent": "PULL_REQUEST_REVIEW_COMMENT",
+    "DeleteEvent": "DELETE",
+    "PushEvent": "PUSH",
+    "PullRequestEvent": "PULL_REQUEST"
+}
--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log("Hello World!")
+import { GITHUB_ACTION_TYPE } from "./constant.js"
+
+async function main() {
+  process.stdin.on("data", async (data) => {
+    const input = data.toString().trim();
+
+    const command = input.split(" ")[0];
+
+    console.log("Input: ", input);
+
+    switch (command) {
+      case "github-activity":
+        const username = input.split(" ")[1];
+        await fetchUser(username)
+        break;
+      case "exit":
+        process.exit();
+        break;
+      default:
+        console.log("Unknown command.");
+    }
+  });
+}
+
+main();
+
+fetchUser = async (username) => {
+  fetch(`https://api.github.com/users/${username}/events`)
+    .then(async (data) => {
+        const response = await data.json()
+
+        if(response.length == 0) {
+            console.log("Username not found...")
+            return   
+        }
+
+        let events = []
+
+        for(let i = 0; i < response.length; i++) {
+            let repository = response[i]?.repo?.id
+            let type = response[i]?.type
+
+            filterByTypeAndRepo()
+
+        }
+
+        console.log(response)
+    })
+    .catch((error) => {
+      console.log("Something went wrong: ", error);
+    });
+};
+
+const filterByTypeAndRepo = (events, repo, type) => {
+    return events.filter((e) => e.repo == repo && e.type == type)
+}
